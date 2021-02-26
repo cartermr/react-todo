f7f149b6614264706912cd3fe21c5bc5b3849357
Matt Carter <carter.matthew@me.com>
2021-02-25 21:52:44 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -16,19 +16,30 @@ class App extends React.Component {
   }
 
   handleClick = () => {
-    this.setState({todos: [...this.state.todos, this.state.input]})
-    this.setState({input: ''})
-    console.log(this.state.todos)
+    this.setState({todos: [...this.state.todos, this.state.input],
+    input: ''})
   }
 
-  render() {    
-    return (
-      <div className="App">
-        <label>Enter a New Todo</label>
-        <input type='text' value={this.state.input} onChange={this.handleChange}></input>
-        <button onClick={this.handleClick}>Enter Todo</button>
-      </div>
-    );
+  handleCompleted = (e) => {
+    this.setState({todos: this.state.todos.filter( todo => todo != e.target.name)})
+  }
+
+  render() {
+      return (
+          <div className='main'>
+            <div className='App'>
+              <label>Enter a New Todo</label>
+              <input type='text' value={this.state.input} onChange={this.handleChange}></input>
+              <button onClick={this.handleClick}>Enter Todo</button>
+            </div>
+            <div className='App'>
+              <label>Todo List</label>
+              <ol>
+                {this.state.todos.map((todo, index) => <li key={index}>{todo}<button name={todo} className="listBtn" onClick={this.handleCompleted}>Completed</button></li>)}
+              </ol>
+            </div>
+          </div>
+      )
   }
 }
 
